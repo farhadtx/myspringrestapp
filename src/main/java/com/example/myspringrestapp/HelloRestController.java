1488204ed8de1d90a94e8a1d0e5fec4a375db299
farhad <farhad@tx.company>
2022-04-11 14:35:46 +0300

    Added /ping endpoint

--- src/main/java/com/example/myspringrestapp/HelloRestController.java
+++ src/main/java/com/example/myspringrestapp/HelloRestController.java
@@ -5,9 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRestController {
-    
+
     @GetMapping("/hello")
     public String hello(){
         return "Hello";
+    }
+
+    @GetMapping("/ping")
+    public String ping(){
+        return "pong";
     }
 }
