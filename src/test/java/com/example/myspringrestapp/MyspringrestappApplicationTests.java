56c217ea421ebb96011f9a9beda902925352c4d0
farhad <farhad@tx.company>
2022-04-11 14:11:13 +0300

    first commit

---
+++ src/test/java/com/example/myspringrestapp/MyspringrestappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myspringrestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringrestappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
