f7a18052a34f370f3320ab002a1ead3f8a1a9e36
farhad <farhad@tx.company>
2022-04-12 11:08:00 +0300

    Removed unnecessary APIs

--- src/main/java/com/example/myspringrestapp/HelloRestController.java
+++ src/main/java/com/example/myspringrestapp/HelloRestController.java
@@ -15,20 +15,4 @@ public class HelloRestController {
     public String ping(){
         return "pong";
     }
-
-    @GetMapping("docker-push")
-    public String dockerPush() {
-        return "docker-push-test";
-    }
-
-    @GetMapping("kubernetes-test")
-    public String kubeTest(){
-        return "Kube-Test";
-    }
-
-    @GetMapping("k8s-ci-cd")
-    public String k8scicd(){
-        return "K8s CI CD working";
-    }
-
 }
