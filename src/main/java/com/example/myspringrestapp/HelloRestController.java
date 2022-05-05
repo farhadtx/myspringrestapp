2ab53ffc4b844a6cee1cb6f6d6412b8fcdee93a2
farhad <farhad@tx.company>
2022-05-05 09:29:35 +0300

    Updated some test api

--- src/main/java/com/example/myspringrestapp/HelloRestController.java
+++ src/main/java/com/example/myspringrestapp/HelloRestController.java
@@ -8,7 +8,7 @@ public class HelloRestController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "Hello";
+        return "Hello from my CI/CD pipeline";
     }
 
     @GetMapping("/ping")
